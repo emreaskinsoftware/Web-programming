0859f32265f65e014b8065ef6a946e57d46f1894
emreaskinsoftware <emreaskinoffical@gmail.com>
2026-02-24 15:32:04 +0300

    first commit

---
+++ web-lab-1/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+     <h1>Web Tasarimi ve Programlama </h1>
+     <h2>LAB -1</h2>
+     <p>Ad Soyad: Emre Aşkın</p>
+     <p>Ogrenci No: 230541146</p>
+    </div>
+     )
+    
+}
+
+export default App
