38f35faf19e4199253e8a2227872e9bbd78bcfb7
emreaskinsoftware <emreaskinoffical@gmail.com>
2026-02-24 15:59:18 +0300

    Lab2 first commit

--- web-lab-1/src/App.jsx
+++ web-lab-1/src/App.jsx
@@ -7,14 +7,38 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-     <h1>Web Tasarimi ve Programlama </h1>
-     <h2>LAB -1</h2>
-     <p>Ad Soyad: Emre Aşkın</p>
-     <p>Ogrenci No: 123456789</p>
-     <p>Hobiler: Kitap okumak</p>
-     <p>İletişim: 0123456789</p>
-    </div>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#hakkimda">Hakkımda</a></li>
+            <li><a href="#projeler">Projeler</a></li>
+            <li><a href="#iletisim">İletişim</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <section id="hakkimda">
+          <h2>Hakkımda</h2>
+          Yazılım Mühendisliği 3. sınıf öğrencisiyim. Araştırma yapmayı ve web uygulamaları geliştirmeyi severim.
+        </section>
+
+        <section id="projeler">
+          <h2>Projelerim</h2>
+          BERT ile Smishing tespiti
+        </section>
+
+        <section id="iletisim">
+          <h2>İletişim</h2>
+          123456789
+        </section>
+      </main>
+
+      <footer>
+        <p>&copy; 2025 Emre Aşkın. Tüm hakları saklıdır.</p>
+      </footer>
+    </>
      )
     
 }
